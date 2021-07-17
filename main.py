a40eb893f43d09da60cedb32789807f1cfdc5048
chomp <John2017hype@gmail.com>
2021-07-17 14:49:56 -0500

    m

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import pygame
+import sys
+
+pygame.init()
+
+# variables
+maxFPS = 60
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((500, 500))
+
+# updating screen
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill((90, 219, 124))
+    pygame.display.update()   
+    clock.tick(maxFPS)     
