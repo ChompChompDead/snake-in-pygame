3853dbafcc5e6af960a7cf35d764b8c3eb1669be
SmileMan1235 <76782747+SmileMan1235@users.noreply.github.com>
2021-07-19 21:13:32 +0100

    Added Fruit

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
 import sys
-
+import random
+from pygame.math import Vector2
 pygame.init()
+
+#fruit class
+class Fruit:
+    def __init__(self):
+        self.x = random.randint(0, cellAmount - 1)
+        self.y = random.randint(0, cellAmount - 1)
+        self.position = Vector2(self.x, self.y)
+
+    def createFruit(self):
+        fruitShape = pygame.Rect(int(self.position.x * cellSize), int(self.position.y * cellSize), cellSize, cellSize)
+        pygame.draw.rect(screen,(255,65,20), fruitShape)
 
 # variables
 cellSize = 20
@@ -9,6 +21,7 @@ cellAmount = 20
 maxFPS = 60
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((cellAmount * cellSize, cellAmount * cellSize))
+fruit = Fruit()
 
 # title
 pygame.display.set_caption("Snake")
@@ -20,5 +33,6 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill((90, 219, 124))
+    fruit.createFruit()
     pygame.display.update()   
     clock.tick(maxFPS)
